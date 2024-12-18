f599408fa655022d246a43ec162e6886b6b2875f
SocialHippos <dev@socialhippos.com>
2024-12-18 23:19:58 +0500

    pass

--- components/homes/home-ux/hero2.jsx
+++ components/homes/home-ux/hero2.jsx
@@ -1,54 +1,66 @@
 "use client";
 
 import Link from "next/link";
-import { Autoplay, Navigation } from "swiper/modules";
+import { Autoplay, Navigation, EffectFade } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import required Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+import 'swiper/css/autoplay';
 
 const slidesData = [
   {
     backgroundImage:
-      'url("/images/main-slider/slide6-1.jpg") center center no-repeat',
-    title: "Solar Wind Power Technology",
+      'url("/images/banner/banner1.png") center center no-repeat',
+    title: "FLAWLESS SKIN",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    buttons: ["ABOUT US", "SERVICES"],
+    buttons: ["ABOUT US"],
   },
   {
     backgroundImage:
-      'url("/images/main-slider/slide6-2.jpg") center center no-repeat',
-    title: "Solar Wind Power Technology",
+      'url("/images/banner/banner2.png") center center no-repeat',
+    title: "FLAWLESS SKIN",
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-    buttons: ["ABOUT US", "SERVICES"],
+    buttons: ["ABOUT US"],
   },
 ];
+
 export default function Hero() {
   return (
     <div className="hero-sec">
       <Swiper
-        modules={[Navigation, Autoplay]}
+        modules={[Navigation, Autoplay, EffectFade]}
         autoplay={{
-          delay: 400000,
+          delay: 4000,
+          disableOnInteraction: false,
         }}
-        speed={250000}
-        loop
-        navigation={{ prevEl: ".snbph6", nextEl: ".snbnh6" }}
-        className=" swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-watch-progress home-slider2 swiper-backface-hidden"
+        effect="fade"
+        fadeEffect={{
+          crossFade: true
+        }}
+        speed={2500}
+        loop={true}
+        navigation={{
+          prevEl: ".snbph6",
+          nextEl: ".snbnh6",
+        }}
+        className="swiper swiper-initialized swiper-horizontal swiper-pointer-events swiper-watch-progress home-slider2 swiper-backface-hidden"
       >
         {slidesData.map((slide, index) => (
           <SwiperSlide
             key={index}
-            className={`swiper-slide slider-content `}
+            className="swiper-slide slider-content"
             style={{ background: slide.backgroundImage }}
           >
             <div className="inner sliderStyle4">
               <h1>{slide.title}</h1>
               <p>{slide.description}</p>
-              <Link href={"/about-1"}>
+              <Link href="/about-1">
                 <button className="site-button">ABOUT US</button>
-              </Link>
-              <Link href={"/services-1"}>
-                <button className="site-button">OUR SERVICES</button>
               </Link>
             </div>
           </SwiperSlide>
@@ -62,7 +74,7 @@ export default function Hero() {
             <i className="las la-angle-right" />
           </div>
         </div>
-      </Swiper>{" "}
+      </Swiper>
     </div>
   );
 }
