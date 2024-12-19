e430173020cbb61be2769d763225faacfa0d56fb
SocialHippos <dev@socialhippos.com>
2024-12-19 23:10:50 +0500

    pass

--- components/Header2.jsx
+++ components/Header2.jsx
@@ -6,77 +6,96 @@ import Image from "next/image";
 import Link from "next/link";
 import { socialLinks } from "@/data/socials";
 import { toggleMobileMenu } from "@/utlis/toggleMobileMenu";
+
 export default function Header2() {
   return (
     <header className="site-header mo-left header-transparent header navstyle1">
-      {/* main header */}
       <div className="sticky-header main-bar-wraper navbar-expand-lg">
         <div className="main-bar clearfix">
-          <div className="  clearfix">
-            {/* website logo */}
-            <div className="logo-header mostion ">
-            <Link href="/">
-                <Image alt="" src="/images/skinstinct1.png" width="258" height="75" />
-              </Link>
-            </div>
-            {/* nav toggle button */}
-            <button
-              className="navbar-toggler collapsed navicon justify-content-start"
-              type="button"
-              onClick={toggleMobileMenu}
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDropdown"
-              aria-controls="navbarNavDropdown"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span />
-              <span />
-              <span />
-            </button>
-            {/* extra nav */}
-            <div className="extra-nav border-0">
-              <div className="extra-cell d-flex align-items-center gap-5 gap-xl-3 d-none d-lg-flex">
-                <Link href="/contact">
-                  <button className="button dz-bannerbtn bg-button" type="button">
-                    BOOKING
-                  </button>
-                </Link>
+          <div className="full-width-container clearfix">
+            {/* Desktop header wrapper */}
+            <div className="d-none d-lg-flex align-items-center w-100">
+              {/* Left section with logo and nav */}
+              <div className="d-flex align-items-center">
+                <div className="logo-header me-4">
+                  <Link href="/">
+                    <Image alt="" src="/images/skinstinct-white.svg" width="258" height="75" />
+                  </Link>
+                </div>
+
+                {/* Desktop Navigation */}
+                <div className="header-nav">
+                  <ul className="nav navbar-nav">
+                    <Nav />
+                  </ul>
+                </div>
+              </div>
+
+              {/* Desktop booking button - pushed to right */}
+              <div className="extra-nav border-0 ms-auto">
+                <div className="extra-cell d-flex align-items-center gap-5 gap-xl-3">
+                  <Link href="/contact">
+                    <button className="button dz-bannerbtn bg-button" type="button">
+                      BOOKING
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
 
-            {/* main nav */}
-            <div
-              className="header-nav navbar-collapse collapse justify-content-start"
-              id="navbarNavDropdown"
-            >
-              <div className="logo-header d-md-block d-lg-none">
-              <Link href="/">
-                <Image alt="" src="/images/skinstinct1.png" width="258" height="75" className="invert-logo" />
-              </Link>
+            {/* Mobile header and menu */}
+            <div className="d-lg-none">
+              {/* Mobile header wrapper */}
+              <div className="d-flex justify-content-between align-items-center w-100">
+                <div className="logo-header">
+                  <Link href="/">
+                    <Image alt="" src="/images/skinstinct-white.svg" width="258" height="75" />
+                  </Link>
+                </div>
+                
+                <button
+                  className="navbar-toggler collapsed navicon"
+                  type="button"
+                  onClick={toggleMobileMenu}
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNavDropdown"
+                  aria-controls="navbarNavDropdown"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
               </div>
-              <ul className="nav navbar-nav">
-                <Nav />
-              </ul>
-              <div className="dlab-social-icon">
-                <ul>
-                  {socialLinks.map((elm, i) => (
-                    <React.Fragment key={i}>
-                      <li>
-                        <a
-                          className={`site-button sharp-sm  ${elm.className}`}
-                          href={elm.href}
-                        />
-                      </li>{" "}
-                    </React.Fragment>
-                  ))}
+
+              {/* Mobile menu */}
+              <div
+                className="header-nav navbar-collapse collapse justify-content-start mobile-menu-slide"
+                id="navbarNavDropdown"
+              >
+                <ul className="nav navbar-nav">
+                  <Nav />
                 </ul>
+                <div className="dlab-social-icon">
+                  <ul>
+                    {socialLinks.map((elm, i) => (
+                      <React.Fragment key={i}>
+                        <li>
+                          <Link
+                            className={`site-button sharp-sm ${elm.className}`}
+                            href={elm.href}
+                          />
+                        </li>
+                      </React.Fragment>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* main header END */}
     </header>
   );
 }
